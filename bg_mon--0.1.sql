d43e388536cbd94ef6ca22a47f200bafc563025b
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2017-05-06 11:12:06 +0200

    Refactor UI and remove support on dynamic worker
    * draw basic graphs with Smoothie Charts
    * remove postgres extension functionality
    * fetch bg_mon.html from file instead if compiling it inside

--- bg_mon--0.1.sql
+++
@@ -1,7 +0,0 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION bg_mon" to load this file. \quit
-
-CREATE FUNCTION bg_mon_launch()
-RETURNS pg_catalog.int4 STRICT
-AS 'MODULE_PATHNAME'
-LANGUAGE C;
