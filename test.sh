39ed84abb6245dd9a953e983fcac8a6b5373982e
Alexander Kukushkin <wmd@mail.ru>
2018-09-11 19:36:15 +0200

    Refactor test.sh to make it easier run postgres under valgrind

--- test.sh
+++ test.sh
@@ -8,6 +8,13 @@ set -e
 
 readonly port=5440
 readonly bport=8090
+background_pids=()
+
+function run_bg() {
+    cmd=$1; shift
+    "$cmd" "$@" &
+    background_pids+=($!)
+}
 
 function shutdown_clusters() {
     set +e
@@ -20,8 +27,14 @@ function shutdown_clusters() {
 
 trap shutdown_clusters QUIT TERM INT
 
+function start_postgres() {
+    postgres -D test_cluster$1 --port=$(($port+$1)) &
+    while ! pg_isready -h localhost -p $(($port+$1)); do
+        sleep 1
+    done
+}
+
 function create_cluster() {
-    lport=$(($port+$1))
     initdb test_cluster$1
     echo "host replication all 127.0.0.1/32 trust
 host replication all ::1/128 trust" >> test_cluster$1/pg_hba.conf
@@ -42,7 +55,28 @@ bg_mon.port = $(($bport+$1))" >> test_cluster$1/postgresql.conf
     else
         echo "wal_level = 'hot_standby'" >> test_cluster$1/postgresql.conf
     fi
-    pg_ctl -w -D test_cluster$1 start -o "--port=$lport"
+    start_postgres $1
+}
+
+function curl_ps_loop() {
+    for a in $(seq 1 $2); do
+        curl -s http://localhost:$(($bport+$1))
+        echo
+        sleep 1
+	if [[ ! -z "$3" ]]; then
+            ps auxwwwf | grep postgres
+        fi
+    done
+}
+
+function clone_cluster() {
+    mkdir test_cluster$1
+    chmod 700 test_cluster1
+    if [[ $version =~ ^[1-9][0-9]$ ]]; then opt="-X none"; fi
+    time pg_basebackup $opt -R -c fast -h localhost -p $port -F t -D - | pv -qL 3M | tar -C test_cluster1 -x
+    echo "bg_mon.port = $(($bport+$1))" >> test_cluster$1/postgresql.conf
+    start_postgres $1
+    curl_ps_loop $1 10
 }
 
 create_cluster 0
@@ -51,34 +85,28 @@ echo "create table foo(id int not null primary key);
 INSERT INTO foo values(1);
 BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 SELECT * FROM foo WHERE id = 1;
-select pg_advisory_lock(1), pg_sleep(30)" | psql -h localhost -p $port -d postgres &
+select pg_advisory_lock(1), pg_sleep(30)" | run_bg psql -h localhost -p $port -d postgres
 sleep 1
-psql -h localhost -p $port -d postgres -c "select pg_advisory_lock(1), pg_sleep(5)" &
+run_bg psql -h localhost -p $port -d postgres -c "select pg_advisory_lock(1), pg_sleep(5)"
 sleep 1
-echo -ne "SELECT '\"\\\b\013'\f\t\r\n, pg_advisory_lock(1), pg_sleep(5)" | psql -h localhost -p $port -d postgres &
+echo -ne "SELECT '\"\\\b\013'\f\t\r\n, pg_advisory_lock(1), pg_sleep(5)" | run_bg psql -h localhost -p $port -d postgres
 
-(
-    mkdir test_cluster1
-    chmod 700 test_cluster1
-    if [[ $version =~ ^[1-9][0-9]$ ]]; then opt="-X none"; fi
-    time pg_basebackup $opt -R -c fast -h localhost -p $port -F t -D - | pv -qL 3M | tar -C test_cluster1 -x
-    echo "bg_mon.port = $(($bport+1))" >> test_cluster1/postgresql.conf
-    pg_ctl -w -D test_cluster1 start -o "--port=$(($port+1))"
-    for a in {1..10}; do curl http://localhost:$(($bport+1)) && sleep 1; done
-)&
+run_bg clone_cluster 1
 
 sleep 1
 
 echo "bg_mon.port = $(($bport+3))" >> test_cluster0/postgresql.conf
 pg_ctl -D test_cluster0 reload
 
-curl http://localhost:$(($bport+3))/ui > /dev/null
-( for a in {1..30}; do curl http://localhost:$(($bport+3)) && echo && sleep 1 && ps auxwwwf | grep postgres; done )&
+while ! curl http://localhost:$(($bport+3))/ui > /dev/null; do
+    [[ $((max_attempts++)) -lt 5 ]] && sleep 1 || exit 1
+done
+
+run_bg curl_ps_loop 3 30 1
 
 if [[ $version =~ ^[1-9][0-9]$ ]]; then
     create_cluster 2
-
-    ( for a in {1..30}; do curl http://localhost:$(($bport+2)) && sleep 1; done )&
+    run_bg curl_ps_loop 2 30
 
     psql -h localhost -p $port -d postgres -c "create table test(id serial not null primary key)"
     psql -h localhost -p $(($port+2)) -d postgres -c "create table test(id serial not null primary key)"
@@ -87,6 +115,6 @@ if [[ $version =~ ^[1-9][0-9]$ ]]; then
     psql -h localhost -p $(($port+2)) -d postgres -c "CREATE SUBSCRIPTION mysub CONNECTION 'host=localhost port=$port dbname=postgres' PUBLICATION test"
 fi
 
-wait
+wait ${background_pids[@]}
 
 shutdown_clusters
