b81800d9d45d112ef402eca7588d24364198417b
Alexander Kukushkin <cyberdemn@gmail.com>
2021-10-07 16:09:26 +0200

    Collect discard and flush stats (#44)
    added in 4.19 and 5.5 respectively

--- disk_stats.h
+++ disk_stats.h
@@ -4,7 +4,8 @@
 typedef struct device_stat {
 	char *name;
 	size_t name_len;
-	bool is_used;
+	size_t fields;
+
 	unsigned long read_completed;
 	unsigned long read_merges;
 	unsigned long read_sectors;
@@ -16,29 +17,47 @@ typedef struct device_stat {
 	unsigned int ios_in_progress;
 	unsigned int total_time;
 	unsigned int weighted_time;
+	unsigned long discard_completed;
+	unsigned long discard_merges;
+	unsigned long discard_sectors;
+	unsigned long discard_time;
+	unsigned long flush_completed;
+	unsigned long flush_time;
 
 	double read_merges_diff;
 	double write_merges_diff;
+	double discard_merges_diff;
 	double read_completed_diff;
 	double write_completed_diff;
+	double discard_completed_diff;
+	double flush_completed_diff;
 
 	double read_diff;
 	double write_diff;
+	double discard_diff;
 
 	double average_service_time;
 	double average_queue_length;
 	double average_request_size;
+	double read_average_request_size;
+	double write_average_request_size;
+	double discard_average_request_size;
 	double await;
 	double read_await;
 	double write_await;
+	double discard_await;
+	double flush_await;
 
 	double util;
-
-	bool extended;
 
 	size_t slave_size;
 	char slaves[64];
 } device_stat;
+
+
+#define HAS_EXTENDED_STATS(e) (e.fields >= 12)
+#define HAS_DISCARD_STATS(e) (e.fields >= 16)
+#define HAS_FLUSH_STATS(e) (e.fields >= 18)
 
 typedef struct {
 	unsigned long long uptime;
