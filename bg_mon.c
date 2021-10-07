686a968e80b60b707f3f5d080fb08759749743ad
Rafia Sabih <rafia.sabih@gmail.com>
2021-10-07 16:08:51 +0200

    Show last executed query for idle transactions (#46)
    For the idle transactions show the last query executed from the connection.

--- bg_mon.c
+++ bg_mon.c
@@ -529,10 +529,11 @@ static struct evbuffer *prepare_statistics_output(struct timeval time, system_st
 			}
 #endif
 			if (s.state == STATE_IDLEINTRANSACTION && s.idle_in_transaction_age > 0) {
+				evbuffer_add_printf(evb, ",\"query\":%s", s.query);
 				if (s.idle_in_transaction_age < 10)
-					evbuffer_add_printf(evb, ",\"query\":\"idle in transaction %.2g\"", s.idle_in_transaction_age);
+					evbuffer_add_printf(evb, ",\"idle_in_transaction\":%.2g", s.idle_in_transaction_age);
 				else
-					evbuffer_add_printf(evb, ",\"query\":\"idle in transaction %ld\"", (long)s.idle_in_transaction_age);
+					evbuffer_add_printf(evb, ",\"idle_in_transaction\":%ld", (long) s.idle_in_transaction_age);
 			} else if ((tmp = get_query(s)) != NULL)
 				evbuffer_add_printf(evb, ",\"query\":%s", tmp);
 			evbuffer_add_printf(evb, "}");
